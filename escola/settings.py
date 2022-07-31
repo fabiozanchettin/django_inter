06585a2466074f56290b2486c623dd121d8e2ab5
fabiozanchettin <fabiozan@gmail.com>
2022-07-30 21:36:56 -0400

    sss

--- escola/settings.py
+++ escola/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'escola',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'FaZan#2020',
         'HOST': 'localhost',
         'PORT': '3306',
     }
